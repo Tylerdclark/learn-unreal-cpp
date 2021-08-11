11a8768319ed77504b6c3e5962ba1c5b7cc9b018
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-08-11 14:50:34 -0400

    making const

--- triple-x/tripleX.cpp
+++ triple-x/tripleX.cpp
@@ -5,13 +5,13 @@ int main(int argc, char const *argv[])
     std::cout << "Welcome to the Guessing Game, Triple X!" << std::endl;
     std::cout << "You need to enter the right code to continue..." << std::endl;
 
-    int a{3}, b{6}, c{9};
+    const int a{3}, b{6}, c{9};
 
-    int sum{ a + b + c };
-    int product{ a * b * c};
+    const int sum{ a + b + c };
+    const int product{ a * b * c};
 
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
-    
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Product: " << product << std::endl;
+
     return 0;
 }
